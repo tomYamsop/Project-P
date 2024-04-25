16fc7587b70b2529c2deed052bb7e05c786ed7fe
tomYamsop <zero.user01@mail.ru>
2024-04-25 16:15:48 +0300

    Test_base проверка на соеединение с MAIN-веткой

--- printer/users/forms.py
+++ printer/users/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import AuthenticationForm
 from users.models import User
 
 
-class UserLoginForms(AuthenticationForm):
+class UserLoginForm(AuthenticationForm):
     class Meta:
         model = User
         fields = ('username', 'password')
